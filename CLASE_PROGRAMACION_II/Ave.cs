3d9a04209cf3bbb5abb971ab43b06e8bd9515864
edgardoeg <edgardoeg@yahoo.com>
2025-01-23 22:03:34 -0600

    create ave class

---
+++ CLASE_PROGRAMACION_II/Ave.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace CLASE_PROGRAMACION_II
+{
+    public class Ave
+    {
+        public string? Nombre {  get; set; }
+        public string? Especie { get; set; }
+        public string? Color { get; set; }
+        public double Tamano { get; set; }
+
+        public string? Habitat {  get; set; }
+
+
+        public void Imprimir() 
+        {
+            Console.WriteLine($"Nombre:{Nombre}");
+            Console.WriteLine($"Especia:{Especie}");
+            Console.WriteLine($"Color:{Color}");
+            Console.WriteLine($"Tamano:{Tamano} cm");
+            Console.WriteLine($"Habitat:{Habitat}");
+        }
+
+    }
+}
